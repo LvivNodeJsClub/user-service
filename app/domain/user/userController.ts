817d563c2d9af9aeab38a7ab6fd900be5e6c70e2
Ihor Banadiga <banadiga@gmail.com>
2018-10-09 11:48:47 +0300

    .env.example added import {Request, Response, NextFunction} from "express" added

--- app/domain/user/userController.ts
+++ app/domain/user/userController.ts
@@ -2,32 +2,34 @@ import {UserService} from "./userService";
 
 const HttpStatus = require('http-status-codes');
 
+import {Request, Response} from "express"
+
 export class UserController {
 
     constructor(private userService: UserService) {
     }
 
-    async getAll(request: any, response: any) {
+    async getAll(request: Request, response: Response) {
         const users = await this.userService.getAll();
         response.status(HttpStatus.OK).json(users);
     }
 
-    async get(request: any, response: any) {
+    async get(request: Request, response: Response) {
         const user = await this.userService.get(request.params.id);
         response.status(HttpStatus.OK).json(user);
     }
 
-    async create(request: any, response: any) {
+    async create(request: Request, response: Response) {
         const user = await this.userService.create(request.body);
         response.status(HttpStatus.CREATED).json(user);
     }
 
-    async update(request: any, response: any) {
+    async update(request: Request, response: Response) {
         const user = await this.userService.update(request.params.id, request.body);
         response.status(HttpStatus.OK).json(user);
     }
 
-    async delete(request: any, response: any) {
+    async delete(request: Request, response: Response) {
         await this.userService.delete(request.params.id);
         response.sendStatus(HttpStatus.NO_CONTENT);
     }
--- app/domain/user/userRouter.ts
+++ app/domain/user/userRouter.ts
@@ -4,20 +4,21 @@ import {UserController} from "./userController";
 const wrapAsync = require('app/util/wrapAsync');
 
 const express = require('express');
+import {Request, Response, NextFunction} from "express"
 
 export class UserRouter extends express.Router {
 
     constructor(userValidation: UserValidation, userController: UserController) {
         super();
 
-        const createUserValidator = wrapAsync((request: any, response: any, next: any) => userValidation.createValidator(request, response, next));
-        const updateUserValidator = wrapAsync((request: any, response: any, next: any) => userValidation.updateValidator(request, response, next));
+        const createUserValidator = wrapAsync((request: Request, response: Response, next: NextFunction) => userValidation.createValidator(request, response, next));
+        const updateUserValidator = wrapAsync((request: Request, response: Response, next: NextFunction) => userValidation.updateValidator(request, response, next));
 
-        const getUsers = wrapAsync((request: any, response: any) => userController.getAll(request, response));
-        const getUser = wrapAsync((request: any, response: any) => userController.get(request, response));
-        const createUser = wrapAsync((request: any, response: any) => userController.create(request, response));
-        const updateUser = wrapAsync((request: any, response: any) => userController.update(request, response));
-        const deleteUser = wrapAsync((request: any, response: any) => userController.delete(request, response));
+        const getUsers = wrapAsync((request: Request, response: Response) => userController.getAll(request, response));
+        const getUser = wrapAsync((request: Request, response: Response) => userController.get(request, response));
+        const createUser = wrapAsync((request: Request, response: Response) => userController.create(request, response));
+        const updateUser = wrapAsync((request: Request, response: Response) => userController.update(request, response));
+        const deleteUser = wrapAsync((request: Request, response: Response) => userController.delete(request, response));
 
         this.get('/', getUsers);
         this.get('/:id', getUser);
